9cb32d8c13b56897d9e47a06c31365989f59edae
fedorzvyk <fedorzvyk@gmail.com>
2022-10-06 20:40:45 +0300

    add 1-2 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const arrayList = [];
+
+ingredients.forEach(elem => {
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.textContent = elem;
+    arrayList.push(li);
+});
+
+list.append(...arrayList);
