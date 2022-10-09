8041d4a58f734ff95564fb833eac485fd2b4c90a
fedorzvyk <fedorzvyk@gmail.com>
2022-10-09 13:41:47 +0300

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('button.change-color');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    span.innerText = getRandomHexColor();
+});
