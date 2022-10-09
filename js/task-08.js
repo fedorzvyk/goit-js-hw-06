2fb593b195f24dee0d31fb77d09c9ef0b8d7511e
fedorzvyk <fedorzvyk@gmail.com>
2022-10-09 13:17:24 +0300

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSending);
+
+function onFormSending(event) {
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+
+    event.preventDefault();
+
+    if (email.value && password.value) {
+        formData.forEach((value, name) => {
+            console.log(`${name} : ${value}`);
+        });
+        event.currentTarget.reset();
+    } else {
+        alert('All fields must be filled!!!');
+    }
+}
